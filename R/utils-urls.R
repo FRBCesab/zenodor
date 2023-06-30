4aa33e1f7d85696eb4d2f0fda6bcc28c5269139b
Nicolas Casajus <nicolas.casajus@gmail.com>
2023-06-30 09:27:59 +0200

    fix: handle missing token

--- R/utils-urls.R
+++ R/utils-urls.R
@@ -17,7 +17,14 @@ api_url <- function() "https://zenodo.org/api/records/"
 
 full_raw_url <- function(record_id, token) {
   
-  paste0(raw_url(), record_id, "?token=", token)
+  if (token == "") {
+    
+    return(paste0(raw_url(), record_id))
+    
+  } else {
+    
+    return(paste0(raw_url(), record_id, "?token=", token) )
+  }
 }
 
 
