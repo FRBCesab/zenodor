7ed21a32724bebbaac981965615fba38fc501a4e
Nicolas Casajus <nicolas.casajus@gmail.com>
2023-06-30 08:48:32 +0200

    feat: write urls

---
+++ R/utils-urls.R
@@ -0,0 +1,31 @@
+#' Base URL used to generate cookies
+#' @noRd
+
+raw_url <- function() "https://zenodo.org/record/"
+
+
+
+#' Base URL used to retrieve metadata
+#' @noRd
+
+api_url <- function() "https://zenodo.org/api/records/"
+
+
+
+#' Full URL used to generate cookies
+#' @noRd
+
+full_raw_url <- function(record_id, token) {
+  
+  paste0(raw_url(), record_id, "?token=", token)
+}
+
+
+
+#' Full URL used to retrieve metadata
+#' @noRd
+
+full_api_url <- function(record_id) {
+  
+  paste0(api_url(), record_id)
+}
